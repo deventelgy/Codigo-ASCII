8b350ebfafb6a4ed2b961562ee3bd01bc49d163c
Alin Piero Javier Cruz <alin.javier@entelgy.pe.local>
2023-03-28 16:22:50 -0500

    codigo ascii

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+public class Main {
+    public static void main(String[] args)
+    {
+        String texto=JOptionPane.showInputDialog("Introduce un codigo de la tabla ASCII");
+        //Pasamos el String a int
+        int codigo=Integer.parseInt(texto);
+
+        //Pasamos el codigo a caracter
+        char caracter=(char)codigo;
+
+        System.out.println(caracter);
+    }
+}
